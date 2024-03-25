1293aac5bd3bf4eea76e34892532454b1587021d
Stevie Dean <stephenwdean@gmail.com>
2024-03-24 17:40:48 -0700

    Complete scrapeRmpRatings and getRmpRatings functions.
    Add edgecase support for bad mismatches and names that required special transformations.
    
    Add test for the fetchRMP utils.

--- scripts/fetchRMP.js
+++ scripts/fetchRMP.js
@@ -1,60 +1,202 @@
-const request = require("request");
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const edgecases = require("./edgecases.json");
 
 module.exports = { getRmpRatings };
 
-async function getRmpRatings(profName) {
+async function scrapeRmpRatings(profName, debuggingEnabled = false) {
   profName = profName.replace(" ", "%20");
-  let url = `https://www.ratemyprofessors.com/search/professors/882?q=${profName}`;
+  const url = `https://www.ratemyprofessors.com/search/professors/882?q=${profName}`;
   let teachers = [];
-  console.log("Querying " + url + "...");
+  if (debuggingEnabled) console.log("Querying " + url + "...");
 
-  request(url, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      indexOfTeacher = html.indexOf('"__typename":"Teacher"');
-      while (indexOfTeacher != -1) {
-        // Extract teacher info into JSON object.
-        openingBraceIndex = html.lastIndexOf("{", indexOfTeacher);
-        indexOfSaved = html.indexOf("isSaved", indexOfTeacher);
-        closingBraceIndex = html.indexOf("}", indexOfSaved);
-        teacherInfoString = html.substring(
-          openingBraceIndex,
-          closingBraceIndex + 1
-        );
-        // Log teacher data.
-        teacherData = JSON.parse(teacherInfoString);
-        console.log(
-          "Data extracted: " + teacherData.firstName,
-          teacherData.lastName +
-            "\n" +
-            teacherData.department +
-            "\n" +
-            "Rating: " +
-            teacherData.avgRating +
-            "\n" +
-            "Difficulty: " +
-            teacherData.avgDifficulty +
-            "\n" +
-            "Would take again: " +
-            teacherData.wouldTakeAgainPercent +
-            "%\n"
-        );
-        teachers.push(teacherData);
-        // Find next teacher.
-        indexOfTeacher = html.indexOf(
-          '"__typename":"Teacher"',
-          indexOfTeacher + 1
-        );
-      }
+  const response = await fetch(url);
+  const html = await response.text();
+  let indexOfTeacher = html.indexOf('"__typename":"Teacher"');
+  let indexOfSchool = html.indexOf('"__typename":"School"');
+  // Find SCU school id.
+  let schoolId = -1;
+  while (indexOfSchool != -1) {
+    let openingBraceIndex = html.lastIndexOf("{", indexOfSchool);
+    let closingBraceIndex = html.indexOf("}", indexOfSchool);
+    let schoolInfoString = html.substring(
+      openingBraceIndex,
+      closingBraceIndex + 1
+    );
+    let schoolData = JSON.parse(schoolInfoString);
+    if (schoolData.name == "Santa Clara University") {
+      schoolId = schoolData.id;
+      break;
     }
-  });
+    indexOfSchool = html.indexOf('"__typename":"School"', indexOfSchool + 1);
+  }
+  // If the schoolId is not found, then it means there are no SCU teachers in the query results.
+  if (schoolId == -1) {
+    return teachers;
+  }
+  while (indexOfTeacher != -1) {
+    // Extract teacher info into JSON object.
+    let openingBraceIndex = html.lastIndexOf("{", indexOfTeacher);
+    let indexOfSaved = html.indexOf("isSaved", indexOfTeacher);
+    let closingBraceIndex = html.indexOf("}", indexOfSaved);
+    let teacherInfoString = html.substring(
+      openingBraceIndex,
+      closingBraceIndex + 1
+    );
+    let teacherData = JSON.parse(teacherInfoString);
+    if (debuggingEnabled) console.log(teacherData);
+    // Check if teacher is from SCU.
+    if (teacherData.school.__ref == schoolId) teachers.push(teacherData);
+    // Find next teacher.
+    indexOfTeacher = html.indexOf('"__typename":"Teacher"', indexOfTeacher + 1);
+  }
   return teachers;
 }
 
-readline.question("Enter a professor name: \n", (name) => {
-  getRmpRatings(name);
-  readline.close();
-});
+async function getRmpRatings(rawProfName, debuggingEnabled = false) {
+  let profName = rawProfName.trim();
+  // Empirical testing showed that including middle names in the query does not improve accuracy.
+  // Therefore, we only query by the first first name and the last last name.
+  let realFirstName = profName
+    .substring(0, profName.indexOf(" "))
+    .trim()
+    .toLowerCase();
+  let lastName = profName
+    .substring(profName.lastIndexOf(" "))
+    .trim()
+    .toLowerCase();
+  // Find preferred first name, if it exists.
+  let preferredFirstName = "";
+  let barIndex = profName.indexOf("|");
+  if (barIndex != -1) {
+    preferredFirstName = profName
+      .substring(barIndex + 1, profName.indexOf(" ", barIndex + 2))
+      .trim()
+      .toLowerCase();
+  }
+  let data = null;
+
+  // If this is a special case, query instead by the special name in the edge cases file.
+  for (const transformation of edgecases.name_transformations) {
+    if (
+      realFirstName == transformation.realFirst &&
+      lastName == transformation.realLast
+    ) {
+      data = await scrapeRmpRatings(transformation.rmp, debuggingEnabled);
+    }
+  }
+
+  // Otherwise, query first by last name.
+  data =
+    data == null ? await scrapeRmpRatings(lastName, debuggingEnabled) : data;
+  let entry = null;
+  if (data.length == 1) entry = data[0];
+  if (data.length > 1) {
+    if (debuggingEnabled) console.log("Error: too much data!");
+    for (let j = 0; j < data.length; j++) {
+      if (
+        (data[j].firstName.toLowerCase().trim() == realFirstName ||
+          data[j].firstName.toLowerCase().trim() == preferredFirstName) &&
+        data[j].lastName.toLowerCase().trim() == lastName
+      ) {
+        entry = data[j];
+        break;
+      }
+    }
+  }
+  // Query again using preferred first name if no data found.
+  if (entry == null && preferredFirstName != "") {
+    data = await scrapeRmpRatings(
+      preferredFirstName + " " + lastName,
+      debuggingEnabled
+    );
+    if (data.length == 1) {
+      entry = data[0];
+      if (debuggingEnabled) console.log("Fixed by using preferred first name!");
+    } else if (data.length > 1) {
+      if (debuggingEnabled)
+        console.log("Multiple data after using preferred first name!");
+      for (let j = 0; j < data.length; j++) {
+        if (
+          data[j].firstName == preferredFirstName &&
+          data[j].lastName == lastName
+        ) {
+          if (debuggingEnabled)
+            console.log("Fixed by using preferred first name!");
+          entry = data[j];
+          break;
+        }
+      }
+    }
+  }
+  // Query again using real first name if no data found.
+  if (entry == null) {
+    data = await scrapeRmpRatings(
+      realFirstName + " " + lastName,
+      debuggingEnabled
+    );
+    if (data.length == 1) {
+      entry = data[0];
+      if (debuggingEnabled) console.log("Fixed by using real first name!");
+    } else if (data.length > 1) {
+      if (debuggingEnabled)
+        console.log("Multiple data after using real first name!");
+      for (let j = 0; j < data.length; j++) {
+        if (
+          data[j].firstName == realFirstName &&
+          data[j].lastName == lastName
+        ) {
+          if (debuggingEnabled) console.log("Fixed by using real first name!");
+          entry = data[j];
+          break;
+        }
+      }
+    }
+  }
+  if (debuggingEnabled && entry == null) {
+    console.log("Error: still no data for " + profName);
+  }
+
+  // Check for first name or last name mismatches, and do not return if there is a mismatch.
+  if (entry != null) {
+    const firstNameReceived = entry.firstName.toLowerCase().trim();
+    const lastNameReceived = entry.lastName.toLowerCase().trim();
+    if (
+      !firstNameReceived.includes(realFirstName) &&
+      !realFirstName.includes(firstNameReceived) &&
+      (preferredFirstName == "" ||
+        (preferredFirstName != "" &&
+          !firstNameReceived.includes(preferredFirstName) &&
+          !preferredFirstName.includes(firstNameReceived))) &&
+      !edgecases.not_mismatches.includes(realFirstName + " " + lastName)
+    ) {
+      if (debuggingEnabled)
+        console.log(
+          "Error: first name mismatch\nOurs:",
+          realFirstName,
+          preferredFirstName,
+          lastName,
+          "\nRMP:",
+          firstNameReceived,
+          lastNameReceived
+        );
+      entry = null;
+    }
+    if (
+      !lastNameReceived.includes(lastName) &&
+      !lastName.includes(lastNameReceived) &&
+      !edgecases.not_mismatches.includes(realFirstName + " " + lastName)
+    ) {
+      if (debuggingEnabled)
+        console.log(
+          "Error: last name mismatch\nOurs:",
+          realFirstName,
+          preferredFirstName,
+          lastName,
+          "\nRMP:",
+          firstNameReceived,
+          lastNameReceived
+        );
+      entry = null;
+    }
+  }
+  return entry;
+}
