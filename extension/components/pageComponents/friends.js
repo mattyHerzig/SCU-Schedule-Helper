3450e93ba6fc1fa2a54241f0d580112d5f7b006a
dustin duong <113965223+dustindghb@users.noreply.github.com>
2024-11-01 03:49:08 -0700

    Added next extension
    page routing and some ui
    
    added nextjs extension
    
    page routing and ui for preferences

---
+++ extension/components/pageComponents/friends.js
@@ -0,0 +1,10 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Friends() {
+    return (
+      <Box>
+        <Typography>Friends: </Typography>
+      </Box>
+    );
+}
