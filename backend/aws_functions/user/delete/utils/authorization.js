9f23be807830bdc989337eaf1771b2544eb234e3
Stevie Dean <stephenwdean@gmail.com>
2024-11-06 09:44:52 -0800

    update to use auth handler

---
+++ backend/aws_functions/user/delete/utils/authorization.js
@@ -0,0 +1,44 @@
+import { unauthorizedError } from "../model.js";
+import jwtLib from "jsonwebtoken";
+
+const ERRORS = {
+  NO_HEADER: "no authorization header provided.",
+  BAD_HEADER: "authorization header must provide an issued access token.",
+  INVALID_TOKEN_TYPE: "invalid token type (expected access token)",
+  BAD_ACCESS_TOKEN: "could not verify access token",
+};
+
+export async function handleWithAuthorization(event, context, handler) {
+  const userAuthorization = getUserAuthorization(event);
+  if (!userAuthorization.userId) return unauthorizedError(userAuthorization.authError);
+  return await handler(event, context, userAuthorization.userId);
+}
+
+function getUserAuthorization(event) {
+  if (!event || !event.headers || !event.headers.authorization)
+    return { authError: ERRORS.NO_HEADER };
+  const authorizationHeader = event.headers.authorization;
+  const authType = authorizationHeader.split(" ")[0];
+  if (authorizationHeader.split(" ").length !== 2 || !authType == "Bearer") {
+    return {
+      authError: ERRORS.BAD_HEADER,
+    };
+  }
+  return verifyAccessToken(authorizationHeader.split(" ")[1]);
+}
+
+function verifyAccessToken(accessToken) {
+  try {
+    const token = jwtLib.verify(accessToken, process.env.JWT_SECRET);
+    if (token.type !== "access") {
+      return {
+        authError: ERRORS.INVALID_TOKEN_TYPE,
+      };
+    }
+    return { userId: token.sub };
+  } catch (error) {
+    return {
+      authError: `${ERRORS.BAD_ACCESS_TOKEN} (${error}).`,
+    };
+  }
+}
