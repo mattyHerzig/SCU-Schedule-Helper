d28f809ea0155eacf56d5c0535aa578ff35f962a
Stevie Dean <stephenwdean@gmail.com>
2024-11-09 22:50:13 -0800

    Update authentication.js

--- backend/web_scraping/utils/authentication.js
+++ backend/web_scraping/utils/authentication.js
@@ -22,38 +22,34 @@ export async function authenticate(username, password) {
   let loginTries = 0;
   let needMobileApproval = page.url().includes("duosecurity");
   while (needMobileApproval && loginTries < MAX_LOGIN_TRIES) {
-    try {
-      loginTries++;
-      console.log("***************************ACTION REQUIRED***************************");
-      console.log("Mobile request sent for authentication. Please approve on your phone.");
-      // Press other options, in case the user isn't using mobile push.
-      // Sleep thread
-      const otherOptionsButton = await page.waitForSelector(".other-options-link");
-      await otherOptionsButton.tap();
-      // Wait for the Duo Push button to appear, and tap it.
-      const duoPushButton = await page.waitForSelector("::-p-text(Duo Push)");
-      await duoPushButton.tap();
-      // If there is a verification code, log it.
-      const verificationCodeDiv = await page.$(".verification-code");
-      if (verificationCodeDiv) {
-        const verificationCode = await verificationCodeDiv.evaluate((node) => node.textContent);
-        console.log(`Use verification code: ${verificationCode}`);
-      }
-      // Wait for the Yes, this is my device button to appear (i.e. the user has approved the push).
-      const buttonToTap = await page.waitForSelector(
-        "button::-p-text(Yes, this is my device), button::-p-text(Try again)",
-        { timeout: 65000 }
-      );
-      needMobileApproval = await buttonToTap.evaluate((node) => node.textContent === "Try again");
-      await buttonToTap.tap();
-      if (!needMobileApproval) {
-        await page.waitForRequest((request) => request.url().includes("scu.edu"));
-      } else {
-        console.log("Mobile request not approved. Retrying...");
-        await page.waitForSelector("::-p-text(Other options)");
-      }
-    } catch (error) {
-      console.error(`Error during mobile approval: ${error}`);
+    loginTries++;
+    console.log("***************************ACTION REQUIRED***************************");
+    console.log("Mobile request sent for authentication. Please approve on your phone.");
+    // Press other options, in case the user isn't using mobile push.
+    // Sleep thread
+    const otherOptionsButton = await page.waitForSelector(".other-options-link");
+    await otherOptionsButton.tap();
+    // Wait for the Duo Push button to appear, and tap it.
+    const duoPushButton = await page.waitForSelector("::-p-text(Duo Push)");
+    await duoPushButton.tap();
+    // If there is a verification code, log it.
+    const verificationCodeDiv = await page.$(".verification-code");
+    if (verificationCodeDiv) {
+      const verificationCode = await verificationCodeDiv.evaluate((node) => node.textContent);
+      console.log(`Use verification code: ${verificationCode}`);
+    }
+    // Wait for the Yes, this is my device button to appear (i.e. the user has approved the push).
+    const buttonToTap = await page.waitForSelector(
+      "button::-p-text(Yes, this is my device), button::-p-text(Try again)",
+      { timeout: 65000 }
+    );
+    needMobileApproval = await buttonToTap.evaluate((node) => node.textContent === "Try again");
+    await buttonToTap.tap();
+    if (!needMobileApproval) {
+      await page.waitForRequest((request) => request.url().includes("scu.edu"));
+    } else {
+      console.log("Mobile request not approved. Retrying...");
+      await page.waitForSelector("::-p-text(Other options)");
     }
   }
 
