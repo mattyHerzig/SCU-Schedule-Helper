2258e9c12acc5e893db04a16a9d7ef68d515686e
Stevie <stephenwdean@gmail.com>
2025-02-08 18:42:26 -0800

    WIP improvements to prompting

--- backend/web_scraping/schedule_generation/bulletin_parsing/utils/models.js
+++ backend/web_scraping/schedule_generation/bulletin_parsing/utils/models.js
@@ -3,32 +3,7 @@ import { z } from "zod";
 export const CourseCode = z
   .string()
   .describe(
-    "A course code containing the department code and a number, like CSCI183",
-  );
-
-export const UnitRequirement = z
-  .object({
-    requirementDescription: z
-      .string()
-      .describe("A summary/description of the unit requirement"),
-    numUnitsRequired: z.number().describe("The number of units required"),
-    typeOfUnits: z.enum(["Any", "Upper Division", "Lower Division"]),
-    unitsMustBeFrom: z
-      .enum(["Any", "Department"])
-      .describe(
-        "Where the units must come from (either any school/department, or a specific department)",
-      ),
-    numUnitsFromSCU: z
-      .number()
-      .describe("The number of units that must be taken at SCU, if applicable"),
-    departmentCode: z
-      .string()
-      .describe(
-        "The four-letter department code for the department from which the units must be taken, if applicable.",
-      ),
-  })
-  .describe(
-    "A requirement that specifies a number of units that must be taken, and from where",
+    "A course code containing the 4-letter department code and a course number, with no space in-between and no leading zeroes for the course number, like CSCI183 or MATH14",
   );
 
 export const OtherRequirement = z.object({
@@ -40,19 +15,43 @@ export const OtherRequirement = z.object({
     .describe("A summary/description of the requirement"),
 });
 
+export const CourseOrUnitRequirement = z
+  .object({
+    requirementDescription: z
+      .string()
+      .describe("A summary/description of the unit requirement"),
+    measuredBy: z.enum("courses", "units").describe("How the requirement is measured, either in courses or units"),
+    numRequired: z.number().describe("The number of courses or units required"),
+    type: z.enum(["Any", "Upper Division", "Lower Division"]).describe("The type of courses or units that must be taken, if applicable (otherwise, 'Any')"),
+    numRequiredFromSCU: z
+      .number()
+      .describe("The number of courses or units that must be taken at SCU, if applicable. Otherwise, should be 0."),
+    mustBeFromDepartmentCode: z
+      .string()
+      .describe(
+        "The four-letter department code for the department from which the courses or units must be taken, if applicable. Otherwise, should be blank.",
+      ),
+  })
+  .describe(
+    "A requirement that specifies a number of courses or units that must be taken, and from where.",
+  );
+
+
+
 const COURSE_REQS_DESC =
-  "A valid expression of sets of courses and/or course ranges, with potentially lower/upper bounds and/or department diversity operators, representing the course requirements. Sets can be combined with the & and | operators. CANNOT CONTAIN ANYTHING ELSE, LIKE NATURAL LANGUAGE (THOSE REQUIREMENTS SHOULD GO IN OTHERREQUIREMENTS).";
+  "A valid expression of sets of course s and/or course ranges, with potentially lower/upper bounds and/or department diversity operators, representing the course requirements. Sets can be combined with the & and | operators. CANNOT CONTAIN ANYTHING ELSE, LIKE NATURAL LANGUAGE (THOSE REQUIREMENTS SHOULD GO IN OTHERREQUIREMENTS).";
+
 export const CourseRequirements = z.string().describe(COURSE_REQS_DESC);
 
 export const MajorMinorCourseRequirements = z
   .string()
   .describe(
     COURSE_REQS_DESC +
-      " ALSO SHOULD NOT contain any emphasis requirements. Those should be in the emphasis object.",
+    " ALSO SHOULD NOT contain any emphasis requirements. Those should be in the emphasis object.",
   );
 
 export const UnitRequirements = z
-  .array(UnitRequirement)
+  .array(CourseOrUnitRequirement)
   .describe(
     "Requirements on the number units of a certain type that must be taken, if applicable",
   );
@@ -120,7 +119,72 @@ export const Major = z.object({
     ),
   departmentCode: z
     .string()
-    .describe("The four-letter department code for the major"),
+    .describe(`
+    The four-letter department code for the major. A department code must always be a four-letter, all-caps string, such as CSCI, MATH, or RSOC. It is the string that precedes all course codes within that department. Sometimes, the department code is not explicitly listed on the page. For this reason, here is a list of all department codes and their corresponding subjects:
+ACTG -> Accounting
+AERO -> Aerospace Studies
+AMTH -> Applied Mathematics
+ANTH -> Anthropology
+ARAB -> Arabic
+ARTH -> Art History
+ARTS -> Studio Art
+ASCI -> Arts and Sciences
+ASIA -> Valid for the Asian Studies Minor
+BIOE -> Bioengineering
+BIOL -> Biology
+BUSN -> Business
+CENG -> Civil, Envr & Sust Engineering
+CHEM -> Chemistry
+CHIN -> Chinese
+CHST -> Child Studies
+CLAS -> Classics
+COMM -> Communication
+PSSE -> Posse Scholar
+CSCI -> Computer Science
+CSEN -> Computer Science and Engineering
+DANC -> Dance
+ECEN -> Electrical and Computer Engineering
+ECON -> Economics
+ELSJ -> Experiential Learning for Social Justice
+ENGL -> English
+ENGR -> Engineering
+ENVS -> Environ Studies & Sciences
+ETHN -> Ethnic Studies
+FNCE -> Finance
+FREN -> French & Francophone Studies
+GERM -> German Studies
+GERO -> Valid for the Gerontology minor
+HIST -> History
+HNRS -> Honors Program
+INTL -> Valid for the international studies minor
+ITAL -> Italian Studies
+JAPN -> Japanese Studies
+LEAD -> Lead Scholars Program
+MATH -> Mathematics
+MECH -> Mechanical Engineering
+MGMT -> Management
+MILS -> Military Science
+MKTG -> Marketing
+MUSC -> Music
+NEUR -> Neuroscience
+OMIS -> Information Systems & Analytics
+PHIL -> Philosophy
+PHSC -> Public Health Science
+PHYS -> Physics
+POLI -> Political Science
+PSYC -> Psychology
+RSOC -> Religion & Society
+SCTR -> Scripture & Tradition
+SOCI -> Sociology
+SPAN -> Spanish Studies
+TESP -> Theology Ethics & Spirituality
+THTR -> Theatre
+UGST -> Undergraduate Studies
+UNIV -> University Programs
+WGST -> Women's and Gender Studies
+
+Note that some minors do not have a department, and the department code should be left blank. These types of minors only exist if the page is a non-department page (i.e., the header of the page doesn’t say 'Department of ...'). Instead, these minors come from smaller programs that have one or more directors/advisors, but not an entire department with professors.
+`),
   description: z.string().describe("A summary/description of the major"),
   requiresEmphasis: z
     .boolean()
@@ -171,7 +235,7 @@ export const DepartmentInfo = z.object({
   name: z
     .string()
     .describe(
-      "The name of the department or program, should not include 'department' or 'program'",
+      `The name of the department or program, should not include 'department' or 'program'.`,
     ),
   description: z
     .string()
