bbf949785e1bc90ef10752436eac0add38f597f1
dustin duong <113965223+dustindghb@users.noreply.github.com>
2025-02-25 22:41:59 -0800

    Update patterns.js

--- extension/components/queryComponents/ProfCourseCard/patterns.js
+++ extension/components/queryComponents/ProfCourseCard/patterns.js
@@ -1,3 +1,2 @@
-// patterns.js
 export const courseTakenPattern = /P{(.*?)}C{(.*?)}T{(.*?)}/; // P{profName}C{courseCode}T{termName}
 export const interestedSectionPattern = /P{(.*?)}S{(.*?)}M{(.*?)}/; // P{profName}S{full section string}M{meetingPattern}E{expirationTimestamp}
