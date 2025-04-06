8ba4d4e5086a62c245e1a2f201ba2dac414bf0ef
Stevie Dean <stephenwdean@gmail.com>
2025-04-05 19:47:19 -0700

    remove modal border

--- extension/__test__/react_content_scripts/components/gcal_integration/GoogleCalendarButton.tsx
+++ extension/__test__/react_content_scripts/components/gcal_integration/GoogleCalendarButton.tsx
@@ -104,7 +104,6 @@ export default function GoogleCalendarButton() {
             transform: "translate(-50%, -50%)",
             width: 400,
             bgcolor: "background.paper",
-            border: "2px solid #000",
             boxShadow: 24,
             p: 4,
             borderRadius: "8px",
